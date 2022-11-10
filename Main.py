8fbd80497b1f85fc15ae2c1854bf592dba231633
Nandez22 <112195044+Nandez22@users.noreply.github.com>
2022-11-09 18:53:53 -0600

    added a second button

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ clock = pygame.time.Clock()
 #------------------------------
 
 testButton = Button.txtButton('Test',200,40,'#222222',(150,230),5,5)
-
+button2 = Button.txtButton('Other Button',400,40,'#222222',(200,200),5,5)
 #------------------------------
 
 while True:
@@ -25,6 +25,8 @@ while True:
     screen.fill((121,128,241))
     testButton.draw(screen)
     print(testButton.checkPress(True))
+    button2.draw(screen)
+    button2.checkPress(True)
     #------------------------------
 
     
