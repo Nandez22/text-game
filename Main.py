ebd6fce422190373d05d3b46ab0e4aefbb2d504b
Nandez22 <112195044+Nandez22@users.noreply.github.com>
2022-11-14 23:06:19 -0600

    Added TODO list (shortterm)

--- Main.py
+++ Main.py
@@ -17,3 +17,12 @@ clock = pygame.time.Clock()
 
 Menus.pause(clock)
 img1 = pygame.image.load('Assets/Icons/AI.png').convert_alpha()
+
+
+
+#TODO Add screen size logic
+#TODO Fix overlap issue with dropdown menus
+#TODO Continue outfitting settings menu / UI / UX
+#TODO Possibly create volume slizer
+#TODO Create main game init
+#TODO Start with playerdata json
